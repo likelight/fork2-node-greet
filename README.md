6a841d9a695b9bf4669ed3e08f81cd3f41b75194
likelight <likelight3@gmail.com>
2014-06-28 07:57:46 -0700

    the lesson1 & lesson2

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a series class to study node.js.
+
+The first class is to create an npm package by using npm command like 
+npm link ,npm pack,require,minimist;
---
+++ bin/greet.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+//var greet = require("../lib/index.js");
+//var parseArgs = require("minimist");
+
+//var argv = parseArgs(process.argv.slice(2));
+
+//console.log(greet(argv._[0],argv.drunk));
+
+var command = require("../lib/command");
+command();
+
